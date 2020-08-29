fce728b6219b08258c76e0017fa21a90c7b3afb3
binli <sleticalboy@gmail.com>
2020-08-29 20:25:50 +0800

    [feat] macro supplement.

--- basic/main/macro_type.c
+++ basic/main/macro_type.c
@@ -5,8 +5,10 @@
 #include "macro_type.h"
 
 void macro_main() {
-    message(C, C++);
-    printf("power 3 is %d\n", POWER(3));
+    MSG(C, C++);
+    printf("power 3 is %d\n", POWER(3 + 3));
+    printf("the max between 13 and 9 is %d\n", MAX(13 + 2, 9));
     printf("file is %s\n", __FILE__);
-    printf("file name is %d\n", __LINE__);
+    printf("this code in file is line %d\n", __LINE__);
+    printf("date and time %s %s\n", __DATE__, __TIME__);
 }
--- basic/main/macro_type.h
+++ basic/main/macro_type.h
@@ -4,10 +4,20 @@
 
 #include <stdio.h>
 
-// 定义宏
-#define message(a, b) \
+#ifndef MSG
+// 定义宏， 其中 # 叫做字符串常量化运算符，在将宏参数转换成字符串是才会用到
+#define MSG(a, b) \
     printf(#a " and " #b ": we love you!\n")
+#endif
 
-#define POWER(num) (num * num)
+#ifndef POWER
+// 定义宏：计算一个数的平方
+#define POWER(num) ((num) * (num))
+#endif
+
+#ifndef MAX
+// 定义宏：求两数中的最大值
+#define MAX(x, y) (x > y ? x : y)
+#endif
 
 void macro_main();
